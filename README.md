464bfb3779dfdd87a3da0570b8b4b1a91b419c41
Yadickson Soto <yadickson.soto@basedos.cl>
2017-11-10 15:51:38 -0300

    Updatefirst support

--- README.md
+++ README.md
@@ -1,2 +1,228 @@
 # generator-ajslib
-Yeoman Gulp Generator for AngularJS Library
+> Yeoman Gulp generator for AngularJS Library
+
+[![TravisCI Status][travis-image]][travis-url]
+[![Coverage Status][coveralls-image]][coveralls-url]
+[![dependencies][dependencies-image]][dependencies-url]
+[![dev-dependencies][dev-dependencies-image]][dev-dependencies-url]
+[![Known Vulnerabilities][vulnerabilities-image]][vulnerabilities-url]
+[![npm version][npm-image]][npm-url]
+
+## Installation
+
+First, install [Yeoman](http://yeoman.io) and generator-ajslib using [npm](https://www.npmjs.com/) (we assume you have pre-installed [node.js](https://nodejs.org/)).
+
+```bash
+npm install -g yo
+npm install -g generator-ajslib
+```
+
+## List generators
+
+```bash
+yo --generators
+```
+
+## Make project
+
+Help
+
+```bash
+yo ajslib --help
+```
+
+Then generate your new project:
+
+```bash
+yo ajslib
+```
+
+## Getting To Know Yeoman
+
+ * Yeoman has a heart of gold.
+ * Yeoman is a person with feelings and opinions, but is very easy to work with.
+ * Yeoman can be too opinionated at times but is easily convinced not to be.
+ * Feel free to [learn more about Yeoman](http://yeoman.io/).
+
+## Usage
+
+Make a new directory, and `cd` into it:
+
+```
+mkdir project && cd project
+```
+
+Run `yo ajslib`, optionally passing an app name:
+```
+yo ajslib [project-name]
+```
+## Build develop mode
+
+Make build directory with full application
+
+```
+gulp build
+```
+
+## Dist production mode
+
+Make dist directory with minimal application for production
+
+```
+gulp dist
+```
+
+## Test
+
+> Console
+
+```
+gulp test
+```
+
+> Coverage
+
+```
+Check coverage directory
+```
+
+## Clean
+
+```
+gulp clean
+```
+
+## Docs
+
+```
+gulp docs
+```
+
+## Generators
+
+Available generators:
+
+* [ajslib](#app) (aka [ajslib:app](#app))
+* [ajslib:directive](#directive)
+* [ajslib:filter](#filter)
+* [ajslib:service](#service)
+* [ajslib:provider](#provider)
+* [ajslib:factory](#factory)
+* [ajslib:value](#value)
+* [ajslib:constant](#constant)
+* [ajslib:decorator](#decorator)
+* [ajslib:component](#component)
+
+
+### App
+Sets up a new AngularJS library
+
+Example:
+```bash
+yo ajslib --help
+yo ajslib project
+```
+
+### Directive
+Generates a directive in `src/directives`.
+
+Example:
+```bash
+yo ajslib:directive --help
+yo ajslib:directive directive-name
+```
+
+### Filter
+Generates a filter in `src/filters`.
+
+Example:
+```bash
+yo ajslib:filter --help
+yo ajslib:filter filter-name
+```
+
+### Service
+Generates an AngularJS service in `src/services`.
+
+Example:
+```bash
+yo ajslib:service --help
+yo ajslib:service service-name
+```
+
+### Provider
+Generates an AngularJS provider in `src/services`.
+
+Example:
+```bash
+yo ajslib:provider --help
+yo ajslib:provider provider-name
+```
+
+### Factory
+Generates an AngularJS factory in `src/services`.
+
+Example:
+```bash
+yo ajslib:factory --help
+yo ajslib:factory factory-name
+```
+
+### Value
+Generates an AngularJS value in `src/services`.
+
+Example:
+```bash
+yo ajslib:value --help
+yo ajslib:value value-name
+```
+
+### Constant
+Generates an AngularJS constant in `src/services`.
+
+Example:
+```bash
+yo ajslib:constant --help
+yo ajslib:constant constant-name
+```
+
+### Decorator
+Generates an AngularJS service decorator in `src/decorators`.
+
+Example:
+```bash
+yo ajslib:decorator --help
+yo ajslib:decorator service-name
+```
+
+### Component
+Generates an AngularJS component in `src/components`.
+
+Example:
+```bash
+yo ajslib:component --help
+yo ajslib:component component-name
+```
+
+## License
+
+GPL-3.0 © [Yadickson Soto](https://github.com/yadickson)
+
+
+[travis-image]: https://travis-ci.org/yadickson/generator-ajslib.svg
+[travis-url]: https://travis-ci.org/yadickson/generator-ajslib
+
+[coveralls-image]: https://coveralls.io/repos/github/yadickson/generator-ajslib/badge.svg
+[coveralls-url]: https://coveralls.io/github/yadickson/generator-ajslib
+
+[dependencies-image]: https://david-dm.org/yadickson/generator-ajslib/status.svg
+[dependencies-url]: https://david-dm.org/yadickson/generator-ajslib?view=list
+
+[dev-dependencies-image]: https://david-dm.org/yadickson/generator-ajslib/dev-status.svg
+[dev-dependencies-url]: https://david-dm.org/yadickson/generator-ajslib?type=dev&view=list
+
+[vulnerabilities-image]: https://snyk.io/package/npm/generator-ajslib/badge.svg
+[vulnerabilities-url]: https://snyk.io/package/npm/generator-ajslib
+
+[npm-image]: https://badge.fury.io/js/generator-ajslib.svg
+[npm-url]: https://badge.fury.io/js/generator-ajslib
--- generators/app/index.js
+++ generators/app/index.js
@@ -2,37 +2,179 @@
 const Generator = require('yeoman-generator');
 const chalk = require('chalk');
 const yosay = require('yosay');
+const mkdirp = require('mkdirp');
+const commandExists = require('command-exists').sync;
+const camelize = require('camelize')
 
 module.exports = class extends Generator {
-  prompting() {
-    // Have Yeoman greet the user.
-    this.log(
-      yosay('Welcome to the cool ' + chalk.red('generator-ajslib') + ' generator!')
-    );
 
-    const prompts = [
-      {
-        type: 'confirm',
-        name: 'someAnswer',
-        message: 'Would you like to enable this option?',
-        default: true
-      }
-    ];
+    constructor(args, opts) {
+        super(args, opts);
 
-    return this.prompt(prompts).then(props => {
-      // To access props later use this.props.someAnswer;
-      this.props = props;
-    });
-  }
+        this.argument('appname', {
+            type: String,
+            required: false,
+            desc: '[Project library name]'
+        });
 
-  writing() {
-    this.fs.copy(
-      this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt')
-    );
-  }
+        this.option('disableConsole', {
+            desc: 'Disable yosay console (default: false)'
+        });
 
-  install() {
-    this.installDependencies();
-  }
+        this.console = !this.options.disableConsole;
+        this.appname = this.options.appname || this.appname;
+    }
+
+    initializing() {
+        this.pkg = require('../../package.json');
+    }
+
+    prompting() {
+        var text = 'Welcome to the ' + chalk.red('Angular-JS') + ' generator!';
+        this.log(this.console ? yosay(text) : text);
+
+        const prompts = [{
+            type: 'input',
+            name: 'name',
+            message: 'Your project name',
+            default: this.config.get('name') || this.appname
+        }, {
+            type: 'input',
+            name: 'description',
+            message: 'Description',
+            default: this.config.get('description')
+        }, {
+            type: 'input',
+            name: 'author',
+            message: 'Author',
+            default: this.config.get('author')
+        }, {
+            type: 'input',
+            name: 'email',
+            message: 'Email',
+            default: this.config.get('email')
+        }, {
+            type: 'input',
+            name: 'license',
+            message: 'License',
+            default: this.config.get('license') || 'GPL-3.0'
+        }, {
+            type: 'input',
+            name: 'username',
+            message: 'What\'s your GitHub username',
+            default: this.config.get('username')
+        }];
+
+        return this.prompt(prompts).then(props => {
+
+            this.name = props.name;
+            this.description = props.description;
+            this.author = props.author;
+            this.email = props.email;
+            this.license = props.license;
+            this.username = props.username;
+
+            this.projectModule = camelize(this.name) + "Module";
+
+            this.config.set('name', this.name);
+            this.config.set('projectModule', this.projectModule);
+            this.config.set('version', '0.0.0');
+            this.config.set('description', this.description);
+            this.config.set('author', this.author);
+            this.config.set('email', this.email);
+            this.config.set('license', this.license);
+            this.config.set('username', this.username);
+            this.config.set('includeBootstrap', this.includeBootstrap);
+            this.config.set('includeSass', this.includeSass);
+
+        });
+    }
+
+    writing() {
+        this._writingReadme();
+        this._writeGulpfile();
+        this._writingPackageJSON();
+        this._writingGitIgnore();
+        this._writingKarmaConfig();
+        this._writingTravisConfig();
+    }
+
+    _writingReadme() {
+        this.fs.copyTpl(
+            this.templatePath('_README.md'),
+            this.destinationPath('README.md'), {
+                name: this.name,
+                description: this.description,
+                author: this.author,
+                license: this.license,
+                username: this.username
+            }
+        );
+    }
+
+    _writeGulpfile() {
+        this.fs.copyTpl(
+            this.templatePath('_gulpfile.js'),
+            this.destinationPath('gulpfile.js'), {
+                date: (new Date).toISOString().split('T')[0],
+                name: this.pkg.name,
+                version: this.pkg.version,
+                includeSass: this.includeSass,
+                includeBootstrap: this.includeBootstrap
+            }
+        );
+    }
+
+    _writingPackageJSON() {
+        this.fs.copyTpl(
+            this.templatePath('_package.json'),
+            this.destinationPath('package.json'), {
+                name: this.name,
+                version: this.version,
+                description: this.description,
+                author: this.author,
+                email: this.email,
+                license: this.license,
+                includeSass: this.includeSass,
+                includeBootstrap: this.includeBootstrap
+            }
+        );
+    }
+
+    _writingGitIgnore() {
+        this.fs.copy(
+            this.templatePath('_gitignore'),
+            this.destinationPath('.gitignore')
+        );
+    }
+
+    _writingKarmaConfig() {
+        this.fs.copy(
+            this.templatePath('_karma.conf.js'),
+            this.destinationPath('karma.conf.js')
+        );
+    }
+
+    _writingTravisConfig() {
+        this.fs.copy(
+            this.templatePath('_travis.yml'),
+            this.destinationPath('.travis.yml')
+        );
+    }
+
+    install() {
+        const hasYarn = commandExists('yarn');
+
+        this.installDependencies({
+            npm: !hasYarn,
+            bower: false,
+            yarn: hasYarn
+        });
+
+        var text = 'Run ' + chalk.red('npm [options [build, dist, test, docs]]');
+        this.log(this.console ? yosay(text) : text);
+
+        text = 'Run ' + chalk.red('yo ajslib --help');
+        this.log(this.console ? yosay(text) : text);
+    }
 };
